df521c04f1f772378ec8e3364ef52dbb4f1566f2
ratakoolta <ratakoolta@gmail.comgit config --local user.name ratakooltagit config --global user.email ratakoolta@gmail.com>
2022-12-15 16:33:00 -0400

    Specification 1 Done
    Specification 1, 1.1, 1.2, 1.3 DONE

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+import markdown
 
 from . import util
 
+def transl_md_html(title):
+    content = util.get_entry(title)
+    markdowner = markdown.Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, title):
+    html_content = transl_md_html(title)
+    if html_content == None:
+        return render(request, "encyclopedia/null.html")
+    else: 
+        return render(request, "encyclopedia/entry.html", {
+            "titulo": title,
+            "contenido": html_content
+        })
